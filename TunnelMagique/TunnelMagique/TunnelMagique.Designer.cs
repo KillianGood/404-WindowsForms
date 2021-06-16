6a0ae6f3b1f7db1f3a9a9b37ea5f5e2b5a6e5053
KillianGood <61836895+KillianGood@users.noreply.github.com>
2021-06-16 08:06:47 +0200

    adding tunnel magique

---
+++ TunnelMagique/TunnelMagique/TunnelMagique.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace TunnelMagique
+{
+    partial class TunnelMagique
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PnlContainer = new System.Windows.Forms.Panel();
+            this.BtnUp = new System.Windows.Forms.Button();
+            this.BtnRight = new System.Windows.Forms.Button();
+            this.BtnDown = new System.Windows.Forms.Button();
+            this.BtnLeft = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // PnlContainer
+            // 
+            this.PnlContainer.BackColor = System.Drawing.Color.White;
+            this.PnlContainer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PnlContainer.Location = new System.Drawing.Point(28, 27);
+            this.PnlContainer.Name = "PnlContainer";
+            this.PnlContainer.Size = new System.Drawing.Size(250, 250);
+            this.PnlContainer.TabIndex = 0;
+            // 
+            // BtnUp
+            // 
+            this.BtnUp.Location = new System.Drawing.Point(130, 310);
+            this.BtnUp.Name = "BtnUp";
+            this.BtnUp.Size = new System.Drawing.Size(50, 50);
+            this.BtnUp.TabIndex = 1;
+            this.BtnUp.Text = "Up";
+            this.BtnUp.UseVisualStyleBackColor = true;
+            this.BtnUp.Click += new System.EventHandler(this.BtnUp_Click);
+            // 
+            // BtnRight
+            // 
+            this.BtnRight.Location = new System.Drawing.Point(179, 359);
+            this.BtnRight.Name = "BtnRight";
+            this.BtnRight.Size = new System.Drawing.Size(50, 50);
+            this.BtnRight.TabIndex = 2;
+            this.BtnRight.Text = "Right";
+            this.BtnRight.UseVisualStyleBackColor = true;
+            this.BtnRight.Click += new System.EventHandler(this.BtnRight_Click);
+            // 
+            // BtnDown
+            // 
+            this.BtnDown.Location = new System.Drawing.Point(130, 408);
+            this.BtnDown.Name = "BtnDown";
+            this.BtnDown.Size = new System.Drawing.Size(50, 50);
+            this.BtnDown.TabIndex = 3;
+            this.BtnDown.Text = "Down";
+            this.BtnDown.UseVisualStyleBackColor = true;
+            this.BtnDown.Click += new System.EventHandler(this.BtnDown_Click);
+            // 
+            // BtnLeft
+            // 
+            this.BtnLeft.Location = new System.Drawing.Point(80, 359);
+            this.BtnLeft.Name = "BtnLeft";
+            this.BtnLeft.Size = new System.Drawing.Size(50, 50);
+            this.BtnLeft.TabIndex = 4;
+            this.BtnLeft.Text = "Left";
+            this.BtnLeft.UseVisualStyleBackColor = true;
+            this.BtnLeft.Click += new System.EventHandler(this.BtnLeft_Click);
+            // 
+            // TunnelMagique
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(311, 481);
+            this.Controls.Add(this.BtnLeft);
+            this.Controls.Add(this.BtnDown);
+            this.Controls.Add(this.BtnRight);
+            this.Controls.Add(this.BtnUp);
+            this.Controls.Add(this.PnlContainer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "TunnelMagique";
+            this.Text = "Tunnel Magique";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PnlContainer;
+        private System.Windows.Forms.Button BtnUp;
+        private System.Windows.Forms.Button BtnRight;
+        private System.Windows.Forms.Button BtnDown;
+        private System.Windows.Forms.Button BtnLeft;
+    }
+}
+
---
+++ TunnelMagique/TunnelMagique/TunnelMagique.cs
@@ -0,0 +1,157 @@
+﻿///ETML
+///Auteur : Anthony Höhn
+///Date : 14.06.2021
+///Description : Tunnel magique, grid made up of 25x25 labels.
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TunnelMagique
+{
+    public partial class TunnelMagique : Form
+    {
+        /// <summary>
+        /// Constant : The number of labels in axe x
+        /// </summary>
+        private const int PANEL_WIDTH = 5;
+
+        /// <summary>
+        /// Constant : The number of labels axe y
+        /// </summary>
+        private const int PANEL_HEIGHT = 5;
+
+        /// <summary>
+        /// Constant :  The width of the labels
+        /// </summary>
+        private const int LABEL_WIDTH_SIZE = 50;
+
+        /// <summary>
+        /// Constant : The height of the labels
+        /// </summary>
+        private const int LABEL_HEIGHT_SIZE = 50;
+
+        public int CURSOR_POSITION_X = 100;
+        public int CURSOR_POSITION_Y = 100;
+
+
+        private Point position = new Point(100, 100);
+        Control currentLabel;
+
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public TunnelMagique()
+        {
+            InitializeComponent();
+
+            //create labels in panel
+            for (int i = 0; i < 5; i++)
+            {
+
+                for (int j = 0; j < 5; j++)
+                {
+                    Label lbl = new Label();
+
+                    lbl.Width = LABEL_WIDTH_SIZE;
+                    lbl.Height = LABEL_HEIGHT_SIZE;
+
+                    lbl.Location = new Point(i * LABEL_WIDTH_SIZE, j * LABEL_HEIGHT_SIZE);
+
+
+                    //border labels red
+                    if (lbl.Location.X == 0 || lbl.Location.X == 200 || lbl.Location.Y == 0 || lbl.Location.Y == 200)
+                    {
+                        lbl.BackColor = Color.Red;
+                        lbl.Tag = "Red";
+
+                    }
+
+                    //midle white line
+                    if (lbl.Location.X == 100 || lbl.Location.Y == 100)
+                    {
+                        lbl.BackColor = Color.White;
+
+                    }
+
+                    // Middle Cursor
+                    if (lbl.Location.Y == 100 && lbl.Location.X == 100)
+                    {
+                        lbl.BackColor = Color.Yellow;
+                    }
+                    PnlContainer.Controls.Add(lbl);
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// move the yellow point Up
+        /// </summary>
+        /// <param name="sender">object that causes the event</param>
+        /// <param name="e">event-related data</param>
+        private void BtnUp_Click(object sender, EventArgs e)
+        {
+            currentLabel = PnlContainer.GetChildAtPoint(position);
+            if (currentLabel.Location.Y > 0)
+            {
+                currentLabel.BackColor = Color.White;
+                position.Y -= 50;
+                currentLabel = PnlContainer.GetChildAtPoint(position);
+                currentLabel.BackColor = Color.Yellow;
+            }
+        }
+
+        /// <summary>
+        /// move the yellow point Right
+        /// </summary>
+        /// <param name="sender">object that causes the event</param>
+        /// <param name="e">event-related data</param>
+        private void BtnRight_Click(object sender, EventArgs e)
+        {
+            currentLabel = PnlContainer.GetChildAtPoint(position);
+            if (currentLabel.Location.X < 200)
+            {
+                currentLabel.BackColor = Color.White;
+                position.X += 50;
+                currentLabel = PnlContainer.GetChildAtPoint(position);
+                currentLabel.BackColor = Color.Yellow;
+            }
+        }
+
+        /// <summary>
+        /// move the yellow point Down
+        /// </summary>
+        /// <param name="sender">object that causes the event</param>6
+        /// <param name="e">event-related data</param>
+        private void BtnDown_Click(object sender, EventArgs e)
+        {
+            currentLabel = PnlContainer.GetChildAtPoint(position);
+            if (currentLabel.Location.Y < 200)
+            {
+                currentLabel.BackColor = Color.White;
+                position.Y += 50;
+                currentLabel = PnlContainer.GetChildAtPoint(position);
+                currentLabel.BackColor = Color.Yellow;
+            }
+        }
+
+        /// <summary>
+        /// move the yellow point Left
+        /// </summary>
+        /// <param name="sender">object that causes the event</param>
+        /// <param name="e">event-related data</param>
+        private void BtnLeft_Click(object sender, EventArgs e)
+        {
+
+            currentLabel = PnlContainer.GetChildAtPoint(position);
+            if (currentLabel.Location.X > 0)
+            {
+                currentLabel.BackColor = Color.White;
+                position.X -= 50;
+                currentLabel = PnlContainer.GetChildAtPoint(position);
+                currentLabel.BackColor = Color.Yellow;
+            }
+        }
+    }
+}
